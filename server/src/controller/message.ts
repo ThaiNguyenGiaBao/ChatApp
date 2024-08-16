81da151121046c5e3f04ab6fa36e1f3b69f5fe0e
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-08-16 11:52:53 +0700

    complete Message route

--- server/src/controller/message.ts
+++ server/src/controller/message.ts
@@ -71,3 +71,68 @@ export const sendMessage = async (req: Request, res: Response) => {
     return res.status(500).json({ message: err.message });
   }
 };
+
+export const getMessages = async (req: Request, res: Response) => {
+  const { id: receiverId } = req.params;
+  const { id: senderId } = req.user;
+
+  const sender = await prisma.user.findUnique({
+    where: {
+      id: senderId,
+    },
+  });
+
+  const receiver = await prisma.user.findUnique({
+    where: {
+      id: receiverId,
+    },
+  });
+
+  console.log("Get ", sender?.username, receiver?.username);
+
+  try {
+    let conversation = await prisma.conversation.findFirst({
+      where: {
+        participantsId: {
+          hasEvery: [senderId, receiverId],
+        },
+      },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+      },
+    });
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    return res.status(200).json(conversation);
+  } catch (err: any) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const getConversations = async (req: Request, res: Response) => {
+  const { id } = req.user;
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        id: {
+          not: id,
+        },
+      },
+      select: {
+        id: true,
+        username: true,
+        profilePic: true,
+      },
+    });
+    return res.status(200).json(users);
+  } catch (err: any) {
+    return res.status(500).json({ message: err.message });
+  }
+};
--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { Request, Response } from "express";
-import { sendMessage } from "../controller/message";
+import {
+  sendMessage,
+  getMessages,
+  getConversations,
+} from "../controller/message";
 import { protectRoute } from "../middleware/protectRoute";
 
 const router = express.Router();
-router.post("/send/:id",protectRoute, sendMessage);
+router.post("/send/:id", protectRoute, sendMessage);
+router.get("/conversations", protectRoute, getConversations);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
